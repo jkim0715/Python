2a1bee2569b50c2f26d6247ee4525d3d9290e07a
jkim0715 <gjr191@naver.com>
2019-11-08 09:25:07 +0900

    workshop

---
+++ Day2/workshop.py
@@ -0,0 +1,10 @@
+import time
+import json
+import requests
+
+def request_data_from_url(url)
+    response = requests.get(url)
+    data = response.json
+    return data
+def parse_naver_map_route(data)
+    gil = []    

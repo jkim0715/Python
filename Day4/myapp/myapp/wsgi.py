d832788500b491e929756fda3ef0d730c47d2241
jkim0715 <gjr191@naver.com>
2019-11-11 15:59:13 +0900

    django Intro

---
+++ Day4/myapp/myapp/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for myapp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+#설정 파일 중 하나
+#서비스 배포할 시기에 다룰 것임.
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
+
+application = get_wsgi_application()

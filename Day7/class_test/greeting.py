cf7a3f55f3e36ffc39e31863b442d8af0c5c285e
jkim0715 <gjr191@naver.com>
2019-11-14 18:05:19 +0900

    ORM

---
+++ Day7/class_test/greeting.py
@@ -0,0 +1,10 @@
+from human import Human
+
+
+a = Human'
+a.walk()
+a.say_hello()
+    
+print(a.name)
+a.name = "bamama"
+print(a.name)
---
+++ Day7/class_test/human.py
@@ -0,0 +1,9 @@
+class Human:
+    name = 'name'
+    age= 'age'
+    def say_hello(self):
+        print("hi")
+    def walk(self):
+        print("I am Walking")
+    
+

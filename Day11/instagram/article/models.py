3ca772b3bb3b55b6336efc99e287b2c9553b31e4
jkim0715 <gjr191@naver.com>
2019-11-20 17:29:47 +0900

    Instagram

---
+++ Day11/instagram/article/models.py
@@ -0,0 +1,20 @@
+from django.db import models 
+
+# Create your models here.
+class Article(models.Model):
+    contents = models.TextField()
+    created_date = models.DateTimeField(auto_now_add= True)
+    updated_date = models.DateTimeField(auto_now= True)
+
+    def comments(self):
+        return Comment.objects.filter(article_id=self.id)
+
+
+
+
+
+class Comment(models.Model):
+    contents = models.TextField()
+    created_date = models.DateTimeField(auto_now_add= True)
+    updated_date = models.DateTimeField(auto_now= True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
